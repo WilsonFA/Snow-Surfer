baf635d828af12d0a0745f54ea178f9bd7726412
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2026-03-24 10:43:24 -0300

    Add Level 1 scene and related scripts
    Introduce a new Unity scene 'Level 1' (scene asset, metadata and a GameObject Profile asset) and remove the previous SampleScene. Add gameplay scripts CrashDetector.cs and FinishLine.cs (with their metadata). Update ProjectSettings (EditorBuildSettings and TagManager) to include the new scene and necessary tags/layers.

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class CrashDetector : MonoBehaviour
+{    
+    [SerializeField] float delayBeforeReload = 1f;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor");
+
+        if (collision.gameObject.layer == layerIndex)
+        {
+            Invoke("ReloadScene", delayBeforeReload);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

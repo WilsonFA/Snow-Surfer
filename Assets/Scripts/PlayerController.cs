69ac6e06703388d463654807129fb87eeaaed0f2
Wilson Fernandes (Uilsu) <uilsucontato@gmail.com>
2026-09-06 16:11:37 -0300

    Add boost mechanic and increase camera size
    Introduce a boost mechanic to PlayerController: add serialized baseSpeed and boostSpeed fields, cache a SurfaceEffector2D reference, and add BoostPlayer() (called from Update) to set the effector speed based on vertical input. Refactor rotation into RotatePlayer(). Also add Unity.VisualScripting using and minor variable cleanup. Update scene (Level 1) to include the new speed values on the PlayerController component and increase camera orthographic size from 10 to 12.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float amountTorque = 1f;
+    [SerializeField] float baseSpeed = 15;
+    [SerializeField] float boostSpeed = 20f;
     InputAction moveAction;
     Rigidbody2D myRigidBody2D;
-    
+    SurfaceEffector2D surfaceEffector2D;
+    Vector2 moveVector;
+
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
 
     void Update()
+    {
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
@@ -26,6 +38,20 @@ public class PlayerController : MonoBehaviour
         else if (moveVector.x > 0)
         {
             myRigidBody2D.AddTorque(-amountTorque);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
